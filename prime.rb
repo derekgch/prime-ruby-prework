49413522a61e4440aba8e4b9c6ec07b1d95cc35d
derek chen <derek.g.chen@gmail.com>
2018-03-21 16:46:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 # Add  code here!
 def prime?(n)
-  return true if (n>1&&n<4) || (n<-1&&n>-4)
+  return true if (n>1&&n<4)
   return true if n == 5
+  return false if n <0
   return false if n%2 ==0 || n%3 ==0 || n%5 == 0
+
+
 end
