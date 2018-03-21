20adc626712a4d63c5c3005affaa7b7ab661609a
derek chen <derek.g.chen@gmail.com>
2018-03-21 16:54:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(n)
   return true if (n>1&&n<4)
   return true if n == 5
-  return false if n <0 || n ==1
+  return false if n <2
   return false if n%2 ==0 || n%3 ==0 || n%5 == 0
 
   counter =6
